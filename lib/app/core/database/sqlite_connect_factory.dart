645191a0753bcd87e0e706a37e099cb1f46c6b42
Lucas Ferreira <casferreiraa@gmail.com>
2023-07-31 23:06:55 -0300

    feat(config):init sqlite connection

---
+++ lib/app/core/database/sqlite_connect_factory.dart
@@ -0,0 +1,59 @@
+// * singleton
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:synchronized/synchronized.dart';
+
+class SqliteConnectionFactory {
+  static const _VERSION = 1;
+  static const _DATABASE_NAME = 'todo_list_provider';
+
+  static SqliteConnectionFactory? _instance;
+  Database? _db;
+  final _lock = Lock(); // uma conexão por instância
+
+  SqliteConnectionFactory._(); // contrutor privado
+
+  factory SqliteConnectionFactory() {
+    if (_instance == null) {
+      _instance = SqliteConnectionFactory._();
+    }
+
+    return _instance!;
+  }
+
+  Future<Database> opneConnection() async {
+    var databasePath = await getDatabasesPath();
+    var databasePathFinal = join(databasePath, _DATABASE_NAME);
+
+    //aguardando processos
+    if (_db == null) {
+      await _lock.synchronized(() async {
+        if (_db == null) {
+          _db = await openDatabase(
+            databasePathFinal,
+            version: _VERSION,
+            onConfigure: _onConfigure,
+            onCreate: _onCreate,
+            onUpgrade: _onUpgrade,
+            onDowngrade: _onDowngrade,
+          );
+        }
+      });
+    }
+    return _db!;
+  }
+
+  void closeConnection() {
+    _db?.close();
+    _db = null;
+  }
+
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _onCreate(Database db, int version) async {}
+  Future<void> _onUpgrade(Database db, int oldVersion, int version) async {}
+  Future<void> _onDowngrade(Database db, int oldVersion, int version) async {}
+}
