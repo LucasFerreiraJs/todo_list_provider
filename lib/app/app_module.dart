710711fef3af44d1fc31c297162988f67375c394
Lucas Ferreira <casferreiraa@gmail.com>
2023-08-01 21:40:59 -0300

    feat(migration):initial migration config

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_connect_factory.dart';
 
 // * config genérica da aplicação
 
@@ -12,7 +13,8 @@ class AppModule extends StatelessWidget {
     return MultiProvider(
       providers: [
         Provider(
-          create: (_) => Object(),
+          create: (_) => SqliteConnectionFactory(),
+          lazy: false, // instancia e roda migration assim que o app for iniciado
         )
       ],
       child: AppWidget(),
--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/database/sqllite_adm_connection.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
 
-class AppWidget extends StatelessWidget {
+class AppWidget extends StatefulWidget {
   const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  final sqliAdmConnection = SqliteAdmConnection();
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addObserver(sqliAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance?.removeObserver(sqliAdmConnection);
+  }
 
   @override
   Widget build(BuildContext context) {
