1b8b0f9d197221a6c8957fb6cf07b48dfddc667d
Lucas Ferreira <casferreiraa@gmail.com>
2023-08-08 22:25:15 -0300

    config(register_page): config default_change_notifier

---
+++ lib/app/core/ui/notifier/default_listener_notifier.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_overlay_loader/flutter_overlay_loader.dart';
+import 'package:todo_list_provider/app/core/ui/messages.dart';
+import 'package:todo_list_provider/app/core/ui/notifier/default_change_notifier.dart';
+
+class DefaultListenerNotifier {
+  final DefaultChangeNotifier changeNotifier;
+
+  DefaultListenerNotifier({required this.changeNotifier});
+
+  void listener({
+    required BuildContext context,
+    required SuccessVoidCallback successCallback,
+    ErrorVoidCallback? errorVoidCallback,
+  }) {
+    changeNotifier.addListener(() {
+      if (changeNotifier.loading) {
+        Loader.show(context);
+      } else {
+        Loader.hide();
+      }
+
+      if (changeNotifier.hasError) {
+        if (errorVoidCallback != null) {
+          errorVoidCallback(changeNotifier, this);
+        }
+        Messages.of(context).showError(changeNotifier.error ?? 'Erro interno');
+      } else if (changeNotifier.isSuccess) {
+        successCallback(changeNotifier, this);
+      }
+    });
+  }
+
+  void dispose() {
+    changeNotifier.removeListener(() {});
+  }
+}
+
+typedef SuccessVoidCallback = void Function(
+  DefaultChangeNotifier changeNotifier,
+  DefaultListenerNotifier listenerInstance,
+);
+
+typedef ErrorVoidCallback = void Function(
+  DefaultChangeNotifier changeNotifier,
+  DefaultListenerNotifier listenerInstance,
+);
--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_provider/app/core/exception/auth_exception.dart';
+import 'package:todo_list_provider/app/core/ui/notifier/default_change_notifier.dart';
 import 'package:todo_list_provider/app/services/user/user_service.dart';
 
-class RegisterController extends ChangeNotifier {
+class RegisterController extends DefaultChangeNotifier {
   final UserService _userService;
-  String? error;
-  bool success = false;
 
   RegisterController({required UserService userService}) : _userService = userService;
 
   Future<void> registerUser(String email, String password) async {
     try {
-      error = null;
-      success = false;
+      showLoadingAndResetState();
+
       notifyListeners();
       final user = await _userService.register(email, password);
 
       if (user != null) {
-        success = true;
+        success();
       } else {
-        error = 'Erro ao register usuário';
+        setError('Erro ao register usuário');
       }
     } on AuthException catch (err) {
-      error = err.message;
+      setError(err.message);
     } finally {
+      hideLoading();
       notifyListeners();
     }
-
-    // notifyListeners();
   }
 }
